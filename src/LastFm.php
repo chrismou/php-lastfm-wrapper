e08807402cf5078143925f9fbc807095ce41ba1d
Chris Chrisostomou <dev@mou.me>
2015-12-17 21:02:43 +0000

    initial code & tests import

---
+++ src/LastFm.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Chrismou\LastFm;
+
+class LastFm
+{
+    /**
+     * @var \GuzzleHttp\ClientInterface
+     */
+    protected $httpClient;
+
+    /**
+     * @var string
+     */
+    protected $apiKey;
+
+    /**
+     * @var string
+     */
+    protected $apiSecret;
+
+    /**
+     * @var string
+     */
+    protected $sessionKey;
+
+    /**
+     * @var string
+     */
+    protected $apiUrl = 'http://ws.audioscrobbler.com/2.0/';
+
+    /**
+     * @var string
+     */
+    protected $authUrl = 'http://www.last.fm/api/auth/';
+
+    /**
+     * LastFm constructor.
+     *
+     * @param \GuzzleHttp\ClientInterface $httpClient
+     * @param $apiKey
+     * @param null $apiSecret
+     * @param null $sessionKey
+     */
+    public function __construct(\GuzzleHttp\ClientInterface $httpClient, $apiKey, $apiSecret = null, $sessionKey = null)
+    {
+        $this->httpClient = $httpClient;
+        $this->apiKey = $apiKey;
+        $this->apiSecret = $apiSecret;
+        $this->sessionKey = $sessionKey;
+    }
+
+    /**
+     * @param string $method
+     * @param array $parameters
+     * @param bool $doRequestAuth
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function get($method, $parameters = [], $doRequestAuth = false)
+    {
+        $method = str_replace('_', '.', $method);
+        return $this->doRequest($method, $parameters, $doRequestAuth);
+    }
+
+    /**
+     * @param string $method
+     * @param array $parameters
+     * @param bool $doRequestAuth
+     *
+     * @return \stdClass
+     * @throws \Exception
+     */
+    protected function doRequest($method, $parameters = [], $doRequestAuth = false)
+    {
+        // We automatically append a few parameters here.
+        $parameters = array_merge(
+            [
+                'method' => $method,
+                'format' => 'json',
+                'api_key' => $this->apiKey,
+            ],
+            $parameters
+        );
+
+        // Do we need to authenticate the request ?
+        if ($doRequestAuth) {
+            $parameters = $this->buildParametersForAuthentictatedMethods($parameters);
+        }
+
+        $request = $this->httpClient->post(
+            $this->apiUrl,
+            [
+                'form_params' => $parameters
+            ]
+        );
+
+        $response = json_decode($request->getBody());
+
+        // The JSON couldn't be decoded
+        if ($response === null) {
+            throw new \Exception("Error parsing JSON");
+        }
+
+        // An error has occurred
+        if (!empty($response->error)) {
+            throw new \Exception("[{$response->error}|{$response->message}] "
+                .implode(', ', $response->links) . "\n" . http_build_query($parameters));
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param array $parameters
+     *
+     * @return array
+     */
+    protected function buildParametersForAuthentictatedMethods(array $parameters = [])
+    {
+        if (!empty($this->sessionKey)) {
+            $parameters['sk'] = $this->sessionKey;
+        }
+
+        // Bug : you have to get rid of format parameter to compute the api_sig parameter.
+        // http://www.last.fm/group/Last.fm+Web+Services/forum/21604/_/428269/1#f18907544
+        $fixedParameters = $parameters;
+        unset($fixedParameters['format']);
+
+        ksort($fixedParameters);
+        $signature = '';
+        foreach ($fixedParameters as $k => $v) {
+            $signature .= "$k$v";
+        }
+        $parameters['api_sig'] = md5($signature . $this->apiSecret);
+
+        return $parameters;
+    }
+}
---
+++ tests/LastFmTest.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Chrismou\LastFm\Tests;
+
+use Chrismou\LastFm\LastFm;
+use PHPUnit_Framework_TestCase;
+use \Mockery as m;
+
+class LastFmTest extends PHPUnit_Framework_TestCase
+{
+    /** @var \Mockery\MockInterface */
+    protected $mockHttpClient;
+
+    /** @var \Mockery\MockInterface */
+    protected $mockResponseObject;
+
+    /** @var string */
+    protected $dummyApiKey = '12345';
+
+    /** @var string */
+    protected $dummyApiSecret = 'abcde';
+
+    /** @var array */
+    protected $dummyParameters;
+
+    /** @var string */
+    protected $dummyMethod;
+
+    /** @var string */
+    protected $dummyArtist;
+
+    /** @var \Chrismou\LastFm\LastFm */
+    protected $lastfm;
+
+    public function setUp()
+    {
+        $this->mockHttpClient = m::mock(
+            'GuzzleHttp\ClientInterface',
+            ['post' => null]
+        );
+
+        $this->mockResponseObject = m::mock(
+            'response',
+            ['getBody' => null]
+        );
+
+        $this->dummyMethod = 'artist.getInfo';
+
+        $this->dummyArtist = 'Sam Smith';
+
+        $this->dummyParameters = [
+            'method' => $this->dummyMethod,
+            'format' => 'json',
+            'api_key' => $this->dummyApiKey
+        ];
+
+        $this->lastfm = new LastFm($this->mockHttpClient, $this->dummyApiKey, $this->dummyApiSecret);
+    }
+
+    /**
+     * @test
+     */
+    public function it_makes_a_request()
+    {
+        $this->mockHttpClient->shouldReceive('post')
+            ->once()
+            ->with(
+                'http://ws.audioscrobbler.com/2.0/',
+                [
+                    'form_params' => [
+                        'method' => $this->dummyMethod,
+                        'format' => "json",
+                        'api_key' => $this->dummyApiKey,
+                        'artist' => $this->dummyArtist,
+                    ]
+                ]
+            )
+            ->andReturn($this->mockResponseObject);
+
+        $this->mockResponseObject->shouldReceive('getBody')
+            ->andReturn($this->generateLastfmResponse());
+
+        $this->lastfm->get($this->dummyMethod, ['artist' => $this->dummyArtist]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_adds_auth_parameters_when_required()
+    {
+        $params = [
+            'method' => $this->dummyMethod,
+            'format' => "json",
+            'api_key' => $this->dummyApiKey,
+            'artist' => $this->dummyArtist,
+            'api_sig' => '97ad93796f578cd6e03f90f0819b9a3b',
+        ];
+
+        $this->mockHttpClient->shouldReceive('post')
+            ->once()
+            ->with(
+                'http://ws.audioscrobbler.com/2.0/',
+                [
+                    'form_params' => $params
+                ]
+            )
+            ->andReturn($this->mockResponseObject);
+
+        $this->mockResponseObject->shouldReceive('getBody')
+            ->andReturn($this->generateLastfmResponse());
+
+        $this->lastfm->get($this->dummyMethod, ['artist' => $this->dummyArtist], true);
+    }
+
+    /**
+     * @test
+     */
+    public function it_adds_session_key_parameter_when_set()
+    {
+        $params = [
+            'method' => $this->dummyMethod,
+            'format' => "json",
+            'api_key' => $this->dummyApiKey,
+            'artist' => $this->dummyArtist,
+            'api_sig' => 'dd115d455d62a20fa4d397d6c57721e2',
+            'sk' => 'qwerty',
+        ];
+
+        $this->mockHttpClient->shouldReceive('post')
+            ->once()
+            ->with(
+                'http://ws.audioscrobbler.com/2.0/',
+                [
+                    'form_params' => $params
+                ]
+            )
+            ->andReturn($this->mockResponseObject);
+
+        $this->mockResponseObject->shouldReceive('getBody')
+            ->andReturn($this->generateLastfmResponse());
+
+        $lastfm = new LastFm($this->mockHttpClient, $this->dummyApiKey, $this->dummyApiSecret, 'qwerty');
+
+        $lastfm->get($this->dummyMethod, ['artist' => $this->dummyArtist], true);
+    }
+
+    /**
+     * @test
+     * @expectedException \Exception
+     */
+    public function it_throws_an_exception_when_invalid_json()
+    {
+        $this->mockHttpClient->shouldReceive('post')
+            ->once()
+            ->andReturn($this->mockResponseObject);
+
+        $this->mockResponseObject->shouldReceive('getBody')
+            ->andReturn('notjson');
+
+        $this->lastfm->get($this->dummyMethod, ['artist' => $this->dummyArtist]);
+    }
+
+    /**
+     * @test
+     * @expectedException \Exception
+     */
+    public function it_throws_an_exception_when_api_returns_error()
+    {
+        $this->mockHttpClient->shouldReceive('post')
+            ->once()
+            ->andReturn($this->mockResponseObject);
+
+        $this->mockResponseObject->shouldReceive('getBody')
+            ->andReturn(json_encode([
+                'error' => 'error',
+                'message' => 'The error message',
+                'links' => []
+            ]));
+
+        $this->lastfm->get($this->dummyMethod, ['artist' => $this->dummyArtist]);
+    }
+
+    public function generateLastfmResponse()
+    {
+        $response = [];
+        $response['artist'] = [];
+        $response['artist']['name'] = $this->dummyArtist;
+
+        return json_encode($response);
+    }
+
+    /**
+     * Custom teardown to include mockery expectations as assertions
+     */
+    public function tearDown()
+    {
+        if ($container = \Mockery::getContainer()) {
+            $this->addToAssertionCount($container->mockery_getExpectationCount());
+        }
+
+        \Mockery::close();
+
+        parent::tearDown();
+    }
+}
